e10c9d38e775c2fbac4602521d34ad955ef50bb4
Kirill Seidov <kirse98@gmail.com>
2019-12-21 14:14:48 +0300

    Inventory

---
+++ Assets/DoorOpenDevice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;  //door moving offset
+
+    private bool _open;
+
+    public void Operate()
+    {
+        if (_open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+        _open = !_open;
+    }
+}
---
+++ Assets/Scripts/Charachter/DeviceOperator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceOperator : MonoBehaviour
+{
+    public float radius = 1.5f;     //range for interction with object
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            //get the closiest objects in range of "raduis"
+            Collider[] hitColliders = Physics.OverlapSphere(
+                transform.position,
+                radius);
+            //for each object in range call method "Operate"
+            foreach (Collider hitCollider in hitColliders)
+            {
+                Vector3 direction = hitCollider.transform.position - transform.position;
+                if (Vector3.Dot(transform.forward, direction) > 0.5f)
+                {
+                    hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
